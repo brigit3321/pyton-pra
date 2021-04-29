42a9c7a7a853a1e7bea32a84237aae07b7982f1d
brigit3321 <qqq44cm9@yahoo.co.jp>
2021-04-29 16:46:14 +0900

    アルゴリズム 素数

--- pra.py
+++ pra.py
@@ -1,1 +1,20 @@
-print('pytonとgitの練習ファイル')
+#素数を出力する関数
+
+def prime_numbers(d):
+
+    numbers = []
+
+    for i in range(2, d + 1):
+        flag = True
+        for j in range(2, i):
+            if i % j == 0:
+                flag = False
+                break
+            
+        if flag == True:
+            numbers.append(i)
+
+    return numbers
+
+numbers = prime_numbers(10)
+print(numbers)
